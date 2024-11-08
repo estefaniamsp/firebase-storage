29ae6e6326b3edc172f9e29408f474548daa327d
estefaniamsp <estefania.sanchez01@epn.edu.ec>
2024-11-08 14:32:49 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miappfirebase',
+  webDir: 'www'
+};
+
+export default config;
